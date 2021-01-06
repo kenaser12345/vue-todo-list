80302a29e3165d17a64d2c43427c8b8920859b11
kenaser12345 <kenaser12345@gmail.com>
2021-01-07 03:14:16 +0800

    user authenticate by session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #session
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  #user
+  resource :users, only: [:new, :create]
 end
